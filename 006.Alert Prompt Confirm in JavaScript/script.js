fc46b90032e406011a4ef9300bed5264d1c5e903
Satyam Kumar Jha <satyamjha23112001@gmail.com>
2024-09-02 16:00:42 +0530

    added 6

---
+++ 006.Alert Prompt Confirm in JavaScript/script.js
@@ -0,0 +1,6 @@
+const alertResult = alert("Are you sure?") //it logs undefined.
+const isConfirmed = confirm("Would you like to proceed?")
+console.log(isConfirmed); //it returns true or false
+
+const userInput = prompt("Please enter your name.........................");
+console.log(userInput);  // this gives value entered by user.
