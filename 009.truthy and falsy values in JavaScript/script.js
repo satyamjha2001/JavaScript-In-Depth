97672b844ba4c1acd7837a9ac89826251a8b25f8
Satyam Kumar Jha <satyamjha23112001@gmail.com>
2024-09-04 22:19:26 +0530

    added 9 truthy and falsy

---
+++ 009.truthy and falsy values in JavaScript/script.js
@@ -0,0 +1,8 @@
+//Except these all are truthy values
+
+console.log(Boolean(0));
+console.log(Boolean(-0));
+console.log(Boolean(NaN));
+console.log(Boolean(''));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
