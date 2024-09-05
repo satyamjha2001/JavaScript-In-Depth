3dcbb2de0b3d2d210f88e8b0a44b41c12c29b68f
Satyam Kumar Jha <satyamjha23112001@gmail.com>
2024-09-05 18:57:46 +0530

    added 10 Comparison Operator

---
+++ 010.Comparison operators in JS/script.js
@@ -0,0 +1,36 @@
+const userAge1 = 21;
+const userAge2 = 24;
+
+//== equal to (equality) [it does implicit (automatic) conversion]
+
+console.log(userAge1 == userAge2);
+
+//=== equal value and equal type (strict equality) [you have to do explicit (manual) conversion]
+
+console.log(userAge1 === userAge2);
+
+//!= not equal
+
+console.log(userAge1 != userAge2);
+
+//!== not equal value or not equal type (strict)
+
+console.log(userAge1 !== userAge2)
+
+//> greater than
+
+console.log(userAge1 > userAge2);
+
+//< less than
+
+console.log(userAge1 < userAge2);
+
+//>= greater than or equal to
+
+console.log(userAge1 >= userAge2);
+
+//<= less than or equal to
+
+console.log(userAge1 <= userAge2)
+
+console.log(5>6>7);
